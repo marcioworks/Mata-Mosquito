70533bc9d1198319dbe00e23ab64032f7971c38c
Marcio da Silva Santos <marcio.santos@gruposecrel.com.br>
2019-11-25 08:21:08 -0300

    criação de elemento em posição randomica

--- jogo.js
+++ jogo.js
@@ -5,7 +5,26 @@ function ajustarTamanhoPalco(){
     altura = window.innerHeight
     largura = window.innerWidth
 
-    console.log(altura, largura)
+    console.log(largura,altura)
 }
 
 ajustarTamanhoPalco()
+
+function posicaoRandomica(){
+var posicaoX = Math.floor(Math.random() * largura)- 90
+var posicaoY = Math.floor(Math.random() * altura)- 90
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+console.log(posicaoX,posicaoY)
+
+//criar elemento html
+var mosquito = document.createElement('img')
+mosquito.src = 'imagens/Mosquito.png'
+mosquito.className ='mosquito1'
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute' 
+document.body.appendChild(mosquito)
+}
