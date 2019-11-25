6032d564325868e62c078820943ecde8362f29b0
Marcio da Silva Santos <marcio.santos@gruposecrel.com.br>
2019-11-25 07:31:56 -0300

    projeto criado

---
+++ jogo.js
@@ -0,0 +1,11 @@
+var largura = 0
+var altura = 0
+
+function ajustarTamanhoPalco(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+
+ajustarTamanhoPalco()
